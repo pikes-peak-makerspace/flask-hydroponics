d2d4ca1523104c85a929d8be070c9968f918dc26
Ryan Janke <rjanke.me@gmail.com>
2019-09-21 20:58:02 -0600

    Added modeprobe check every time the route is hit

--- app.py
+++ app.py
@@ -63,6 +63,9 @@ def get_1wire_devices():
         dir_output = "Not on a Raspberry Pi!"
     else: 
         # TODO check if there are no devices and respond accordingly
+        # Update the modprobe devices for changes
+        os.system('modprobe w1-gpio')
+        os.system('modprobe w1-therm')
         # Get a list of the devices
         dir_output = os.listdir(one_wire_devices_path)
         # Remove the master device from the list
